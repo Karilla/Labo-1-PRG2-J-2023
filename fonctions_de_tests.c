f1d8bafb0b7cd1f1c4359a1f0bd189fc37c4c4e5
evarayHEIG <eva.ray@heig-vd.ch>
2023-04-30 14:06:21 +0200

    ajout fichiers .h et .c pour les tests

---
+++ fonctions_de_tests.c
@@ -0,0 +1,18 @@
+/*
+ -----------------------------------------------------------------------------------
+ Nom du fichier : main
+ Auteur(s)      : <A compléter>
+ Date creation  : 30.04.2023
+
+ Description    : <A compléter>
+
+ Remarque(s)    : <A compléter>
+
+ Compilateur    : Mingw-w64 gcc 9.0
+ -----------------------------------------------------------------------------------
+*/
+
+#include "fonctions_de_tests.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
---
+++ fonctions_de_tests.h
@@ -0,0 +1,20 @@
+/*
+ -----------------------------------------------------------------------------------
+ Nom du fichier : main
+ Auteur(s)      : <A compléter>
+ Date creation  : 30.04.2023
+
+ Description    : <A compléter>
+
+ Remarque(s)    : <A compléter>
+
+ Compilateur    : Mingw-w64 gcc 9.0
+ -----------------------------------------------------------------------------------
+*/
+
+#ifndef LABO_1_PRG2_J_2023_FONCTIONS_DE_TESTS_H
+#define LABO_1_PRG2_J_2023_FONCTIONS_DE_TESTS_H
+
+#include <stdlib.h>
+
+#endif //LABO_1_PRG2_J_2023_FONCTIONS_DE_TESTS_H
